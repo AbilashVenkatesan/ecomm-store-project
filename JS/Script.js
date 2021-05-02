b59c9739be55d6a9bd807452c090e7c9774ad98e
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-05-02 17:04:15 -0400

    Creating array

---
+++ JS/Script.js
@@ -0,0 +1,64 @@
+let products = [
+    {
+        toyimg: 'Lego.jpg',
+        toyname: 'Sonic lego set',
+        oldPrice: 49.99,
+        newPrice: 29.99,
+        toyRating: 4.0,
+        toyage: ['5-8', '8-12', '12-15'],
+        toycolor: ['pink', 'white', 'blue'],
+        toysize: ['md', 'lg', 'xl']
+    },
+    {
+        toyimg: 'Chess set.jpg',
+        toyname: 'Chess Set',
+        oldPrice: 89.99,
+        newPrice: 74.99,
+        toyRating: 5.0,
+        toyage: ['2-5', '5-8', '8-12', '12-15', '15-18'],
+        toycolor: ['blue', 'white', 'blue'],
+        toysize: ['sm', 'md', 'lg']
+    },
+    {
+        toyimg: 'Nerf.jpg',
+        toyname: 'Nerf Gun',
+        oldPrice: 219.99,
+        newPrice: 199.99,
+        toyRating: 3.4,
+        toyage: ['5-8', '8-12', '12-15', '15-18'],
+        toycolor: ['yellow', 'white', 'blue'],
+        toysize: ['xs', 'sm', 'md']
+    },
+    {
+        toyimg: 'Rc car.jpg',
+        toyname: 'Rc Car',
+        oldPrice: 349.00,
+        newPrice: 319.99,
+        toyRating: 4.4,
+        toyage: ['8-12', '12-15', '15-18'],
+        toycolor: ['yellow', 'black', 'blue'],
+        toysize: ['sm', 'md', 'lg']
+    },
+    {
+        toyimg: 'Barbie.jpg',
+        toyname: 'Barbie',
+        oldPrice: 50.00,
+        newPrice: 39.99,
+        toyRating: 3.5,
+        toyage: ['2-5', '5-8', '8-12'],
+        toycolor: ['pink', 'white', 'yellow'],
+        toysize: ['sm', 'md', 'lg']
+    },
+    {
+        toyimg: 'Teddy.jpg',
+        toyname: 'Teddy bear set',
+        oldPrice: 24.99,
+        newPrice: 4.0,
+        toyRating: 3.5,
+        toyage: ['2-5', '5-8', '8-12'],
+        toycolor: ['pink', 'white', 'yellow'],
+        toysize: ['sm', 'md', 'lg']
+    }
+
+];
+
